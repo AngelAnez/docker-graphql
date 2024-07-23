56583bb77642fde2fe6661a7fdd5e0e09905c51e
Angel Añez <119618894+AngelAnez@users.noreply.github.com>
2024-07-23 19:07:30 -0400

    Feat: Changed Hello World

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -5,7 +5,7 @@ export class HelloWorldResolver {
 
     @Query( () => String, { description: 'Hola Mundo es lo que retorna', name: 'hello' } )
     helloWorld(): string {
-        return 'Hola Mundo prueba Commit';
+        return 'Hola Mundo prueba Feat';
     }
 
     @Query( () => Float, { name: 'randomNumber' } )
